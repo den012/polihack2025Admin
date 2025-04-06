abd1ccb87a9fbf66ab6ea357c3ad5e0448fea754
Denis <blagadeniss@gmail.com>
2025-04-06 03:29:36 +0300

    added proxy

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   server: {
     proxy: {
-      "/api": "",
+      "/api": "https://255b-188-27-128-167.ngrok-free.app",
     }
   },
   plugins: [
